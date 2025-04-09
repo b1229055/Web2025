768e2401df0a2278211b716db5d02709b6a9e47a
林敬棠 <89287850+b1229055@users.noreply.github.com>
2025-04-09 15:58:55 +0800

    Update script.js

--- LAB9/script.js
+++ LAB9/script.js
@@ -1,40 +1,30 @@
+var openUrl = "https://cloud.culture.tw/frontsite/trans/SearchShowAction.do?method=doFindTypeJ&category=6";
 
-function renderTable(data) {
-    const tbody = document.getElementById("data-body");
-    tbody.innerHTML = ""; 
+var xhr = new XMLHttpRequest();
+xhr.open('GET', openUrl, true);
+xhr.send();
 
-    if (data.length === 0) {
-        tbody.innerHTML = "<tr><td colspan='3'>查無資料</td></tr>";
-        return;
-    }
+xhr.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    dataset = JSON.parse(this.responseText);
+    addNewData(dataset);
+  }
+};
 
-    data.forEach(item => {
-        const row = document.createElement("tr");
-
-        const nameCell = document.createElement("td");
-        nameCell.textContent = item.title || "無名稱";
-
-        const locationCell = document.createElement("td");
-        locationCell.textContent = item.showInfo?.[0]?.location || "無地點";
-
-        const priceCell = document.createElement("td");
-        priceCell.textContent = item.showInfo?.[0]?.price || "無票價";
-
-        row.appendChild(nameCell);
-        row.appendChild(locationCell);
-        row.appendChild(priceCell);
-
-        tbody.appendChild(row);
-    });
+function delOldData() {
+  var myTable = document.getElementById("csie");
+  var rowCount = myTable.rows.length;
+  if (rowCount > 1) {
+    var row = myTable.deleteRow(rowCount - 1);
+  }
 }
 
-fetch("https://cloud.culture.tw/frontsite/trans/SearchShowAction.do?method=doFindTypeJ&category=6")
-    .then(response => response.json())
-    .then(data => {
-        console.log("取得資料:", data);
-        renderTable(data);
-    })
-    .catch(error => {
-        console.error("資料載入失敗:", error);
-        document.getElementById("data-body").innerHTML = "<tr><td colspan='3'>載入失敗</td></tr>";
-    });
+function addNewData(dataset) {
+  var myTable = document.getElementById("csie");
+  dataset.forEach(function(data, index) {
+    var row = myTable.insertRow(-1);
+    row.insertCell(0).innerHTML = data['title'];
+    row.insertCell(1).innerHTML = data['showInfo'][0]['location'];
+    row.insertCell(2).innerHTML = data['showInfo'][0]['price'];
+  });
+}
