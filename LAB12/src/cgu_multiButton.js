81f9aa1ca20ec5288fce59c38f2ac131a1870059
林敬棠 <89287850+b1229055@users.noreply.github.com>
2025-04-30 15:50:37 +0800

    Add files via upload

---
+++ LAB12/src/cgu_multiButton.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import AlarmIcon from '@mui/icons-material/Alarm';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+
+function MultiButton() {
+  const [clickedIndex, setClickedIndex] = useState(null);
+
+  // 只保留 IconButton 部分
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap', marginTop: '20px' }}>
+      <IconButton color="primary" aria-label="add to shopping cart" onClick={() => setClickedIndex('add-to-cart')}>
+        <AddShoppingCartIcon />
+      </IconButton>
+      
+      <IconButton color="primary" aria-label="delete" onClick={() => setClickedIndex('delete')}>
+        <DeleteIcon />
+      </IconButton>
+      
+      <IconButton color="primary" aria-label="add an alarm" onClick={() => setClickedIndex('add-alarm')}>
+        <AlarmIcon />
+      </IconButton>
+      
+
+    </div>
+  );
+}
+
+export default MultiButton;
+
+
+
+
+
+
+
